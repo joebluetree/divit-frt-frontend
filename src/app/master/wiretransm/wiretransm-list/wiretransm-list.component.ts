3eb079a2ddb3fc8af547ab2bbc8b917041d4ee91
alen.c <alen.c@divitsoftlabs.com>
2025-01-24 11:54:20 +0530

    frontend new wiretransfer created

--- src/app/master/wiretransm/wiretransm-list/wiretransm-list.component.ts
+++ src/app/master/wiretransm/wiretransm-list/wiretransm-list.component.ts
@@ -29,9 +29,9 @@ export class WiretransmListComponent extends baseListComponent {
     const param = { id: 0, mode: 'edit', menuid: this.menuid, type: this.type, appid: this.appid };
 
     this.table_data = [
-      { col_name: "edit", col_caption: "EDIT", col_format: "edit", col_sortable: false, col_link: '/marketing/qtnmairEdit', col_param: param, col_show: this.bEdit || this.bView },
+      { col_name: "edit", col_caption: "EDIT", col_format: "edit", col_sortable: false, col_link: '/masters/wiretransmEdit', col_param: param, col_show: this.bEdit || this.bView },
       { col_name: "wtim_id", col_caption: "ID", col_format: "", col_sortable: true, col_link: '', col_param: {}, col_show: true },
-      { col_name: "wtim_refno", col_caption: "REF NO", col_format: "date", col_sortable: true, col_link: '', col_param: {}, col_show: true },
+      { col_name: "wtim_refno", col_caption: "REF NO", col_format: "", col_sortable: true, col_link: '', col_param: {}, col_show: true },
       { col_name: "wtim_to_name", col_caption: "TO", col_format: "", col_sortable: true, col_link: '', col_param: {}, col_show: true },
       { col_name: "wtim_cust_name", col_caption: "CUSTOMER", col_format: "", col_sortable: true, col_link: '', col_param: {}, col_show: true },
       { col_name: "wtim_acc_no", col_caption: "A/C NO", col_format: "", col_sortable: true, col_link: '', col_param: {}, col_show: true },
