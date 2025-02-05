c4bd900907a22f63a3bd29b28080b88e4fdfd66b
sourav.v <sourav.v@divitsoftlabs.com>
2025-02-05 14:19:42 +0530

    showModel added

--- src/app/shared/base-class/baseListComponent.ts
+++ src/app/shared/base-class/baseListComponent.ts
@@ -22,6 +22,7 @@ export abstract class baseListComponent {
   protected bEdit = false;
   protected bView = false;
   protected bDelete = false;
+  protected showModel = true;
 
   protected menum: iMenum | null;
 
